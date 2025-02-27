f544eb698b5273ba05e05aa901a5972383b1b2f1
Maral604 <159859692+Maral604@users.noreply.github.com>
2025-02-28 00:40:26 +0800

    add reference

---
+++ TestCalculator/MemoryAlgorithTest.cs
@@ -0,0 +1,45 @@
+﻿using CalculatorLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace TestCalculator
+{
+    /// <summary>
+    /// Санах ойн нэмэх болон хасах үйлдлүүдийг шалгах тестийн класс.
+    /// </summary>
+    
+    [TestClass]
+    public class MemoryAlgorithTest
+    {
+        /// <summary>
+        /// Санах ойн хадгалсан тоо дээр нэмэх болон хасах үйлдлүүдийг шалгах тест.
+        /// </summary>
+        [TestMethod]
+        public void Add_SubMemoryItem()
+        {
+
+            Memory memory = new Memory();
+            Memoryitem savedItem = memory.Save(4);
+            savedItem.Add(5);
+            savedItem.Sub(7);
+            Assert.AreEqual(1, memory.GetAll().Count);
+            Assert.AreEqual(2, savedItem.SanasanToo);
+        }
+        [TestMethod]
+        public void TestMemoryTotal()
+        {
+            Memory memory = new Memory();
+            memory.Save(3);
+            memory.Save(4);
+            memory.Save(1);
+             double total = 0;
+            foreach (Memoryitem item in memory.memories)
+            {
+                total += item.SanasanToo;
+            }
+            Assert.AreEqual(total, 8);
+        }
+
+
+    }
+}
